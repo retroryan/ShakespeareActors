46243867c987152da0457e62331b2c6be959c37a
retroryan <ryan@grandcloud.com>
2013-06-03 22:40:37 -0600

    teaching monkeys how to write shakespeare

---
+++ app/controllers/Application.scala
@@ -0,0 +1,45 @@
+package controllers
+
+import play.api.mvc._
+
+import play.api.mvc.Action
+
+
+import akka.actor.Props
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+import play.libs.Akka
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+
+  lazy val shakespeare = Akka.system().actorOf(Props[ShakespeareActor], "shakespeare")
+  implicit val timeout = Timeout(5000 milliseconds)
+
+  def writeShakespeare(numberMonkeys:Int) = Action {
+
+    val start = System.nanoTime
+
+    Async {
+      import play.api.libs.concurrent.Execution.Implicits._
+
+      (shakespeare ? numberMonkeys).mapTo[ShakespeareResult].map {
+        shakespeareResult ⇒
+        // We have a result - make some fancy pantsy presentation of it
+          val builder = new StringBuilder
+          builder.append("SHAKESPEARE WORDS:\n")
+          shakespeareResult.shakespeareMagic.foreach { w => builder.append(w + "\n") }
+          builder.append("UNWORTHY WORDS CREATED: " + shakespeareResult.unworthyWords.size + "\n")
+          builder.append("In " + (System.nanoTime - start) / 1000 + "us\n")
+          Ok(builder.toString)
+      }
+    }
+  }
+
+}
---
+++ app/controllers/PlayWriter.scala
@@ -0,0 +1,84 @@
+package controllers
+
+import scala.concurrent.Future
+
+
+import scala.util.Random
+import annotation.tailrec
+
+import akka.actor.{Props, Actor}
+import akka.pattern.ask
+import akka.pattern.pipe
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+
+
+//  copied from https://github.com/henrikengstrom/PlayMiniSample
+//  http://letitcrash.com/post/17888436664/a-sample-application-showcasing-play-mini-and-akka
+
+case class ShakespeareResult(shakespeareMagic: Set[String], unworthyWords: Set[String])
+
+class ShakespeareActor extends Actor {
+
+  import ShakespeareActor._
+
+  lazy val randomGenerator = new Random
+  implicit val timeout = Timeout(5000 milliseconds)
+
+  import context.dispatcher
+
+  def receive = {
+    case actors: Int =>
+      val futures = for (x <- 1 to actors) yield {
+        context.actorOf(Props[MonkeyWorker]) ? randomGenerator.nextInt(100) mapTo manifest[Set[String]]
+      }
+
+      val f = Future.sequence(futures) map {
+        wordSets =>
+          val mergedSet = wordSets reduce ((a, b) => a ++ b)
+          val (shakespeare, unworthy) = mergedSet partition (x => Blueprint.contains(x))
+          ShakespeareResult(shakespeare, unworthy)
+      }
+
+      f pipeTo sender
+  }
+}
+
+
+object ShakespeareActor {
+  lazy val Blueprint = Set("to", "be", "or", "not")
+}
+
+class MonkeyWorker extends Actor {
+
+  import WorkerActor._
+
+  lazy val randomGenerator = new Random
+
+  def receive = {
+    // We simplify the word generation by saying that every word can be between 1 and 26 letters.
+    // If in real life, i.e. when using monkeys, some statistical bias would be useful when handing out the
+    // type writing instructions.
+    case tries: Int =>
+      var words = Set.empty[String]
+      1 to tries foreach {
+        x => words += generateWork(randomGenerator.nextInt(Letters.size))
+      }
+      sender ! words
+  }
+
+  def generateWork(letters: Int) = {
+    @tailrec
+    def trGeneration(letterNumber: Int, result: String): String = letterNumber match {
+      case 0 => result.reverse
+      case n => trGeneration(n - 1, result + Letters(randomGenerator.nextInt(Letters.size - 1)))
+    }
+
+    trGeneration(letters, "")
+  }
+}
+
+object WorkerActor {
+  lazy val Letters = ('a' to 'z').toSeq
+}
